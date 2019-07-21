a1989dd4fdef373317df0e82134d9d620934704d
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-07-21 16:24:00 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 def meal_choice(veg, veggy, meat = "meat")
   val = "A plate of #{meat} with #{veg} and #{veggy}."
-   "What a nutritious meal!"
+  puts "What a nutritious meal!"
    return val
 end
